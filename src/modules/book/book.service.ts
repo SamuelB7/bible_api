9d03c9f0a41ca173755452d8523adfd952f4cdc9
Samuel Belo <belo.samuel@gmail.com>
2022-12-10 14:02:39 -0300

    book module started

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BookService {
+    constructor(
+        
+    ) {}
+
     async findAll() {
         return "Books route"
     }
